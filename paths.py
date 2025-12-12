36e6cf7c8dde7c1d846632b5bb48fb81b20f7fe6
Adrian <s62aweng@uni-bonn.de>
2025-12-12 09:56:36 +0100

    add paths.py

---
+++ paths.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+# Root-Verzeichnisse für Daten und Projekt
+ROOT_LOCAL = Path("/dss/dsstbyfs02/pn49ci/pn49ci-dss-0000/Antartic_Database/git-project/shelf-bench-hedunet/shelfbench-hedunet")
+ROOT_GWS   = Path("/dss/dsstbyfs02/pn49ci/pn49ci-dss-0000/Antartic_Database")
